97c1b54ceed131faa89cbcb646544a06d2ddae6d
MrDongYan <145110343+MrDongYan@users.noreply.github.com>
2023-09-18 19:56:23 +0800

    demo

---
+++ APP.java
@@ -0,0 +1,14 @@
+package com.lit.gui;
+
+public class APP {
+    public static void main(String[] args){
+        // 表示程序的入口
+
+        // 如够我们想要开启一个界面，就创建谁的对象
+//        new LoginJFrame();
+
+        new GameJFrame();
+
+//        new RegisterJFrame();
+    }
+}
